3d0ff45ff34a9e6a64f9e11cf9dbf5f1ea04f8aa
João vitor <joaovitorcfdev@gmail.com>
2024-03-07 23:27:35 -0300

    variaveis

---
+++ conhecendo-a-base/src/olamundo/App.java
@@ -0,0 +1,7 @@
+package olamundo;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Olá, mundo!");
+    }
+}
---
+++ conhecendo-a-base/src/variaveis/VariaveisNotasEstudos.java
@@ -0,0 +1,18 @@
+package variaveis;
+
+/*
+ *  Aula prática sobre variáveis no Java.
+ *  Vriáveis são container de dados.
+ */
+
+public class VariaveisNotasEstudos {
+    public static void main(String[] args) {
+        int dia = 12;
+        int mes = 03;
+        int ano = 2024;
+
+        System.out.println(dia);
+        System.out.println(mes);
+        System.out.println(ano);
+    }
+}
